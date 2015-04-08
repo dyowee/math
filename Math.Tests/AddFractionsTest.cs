804999ae401e384e5cf9b238f344fc67d2a7fc32
Joey Samonte <dyowee23@yahoo.com>
2015-04-08 16:11:13 -0700

    first commit

---
+++ Math.Tests/AddFractionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Math.Tests
+{
+    [TestClass]
+    public class AddFractionsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
